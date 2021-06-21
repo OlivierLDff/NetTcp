0b84d1db6778a805a2b4e7d3051a66ad99c2ba7b
Olivier LDff <olivier.ldff@gmail.com>
2021-06-21 11:43:09 +0200

    ⬆️ QOlm v3.3.1

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -1,6 +1,6 @@
 
 set(QOLM_REPOSITORY "https://github.com/OlivierLDff/QOlm.git" CACHE STRING "QOlm repository url")
-set(QOLM_TAG "557cdbbc44618b4214f3912286191c3e5ac64e21" CACHE STRING "QOlm git tag")
+set(QOLM_TAG "v3.1.1" CACHE STRING "QOlm git tag")
 
 set(SPDLOG_REPOSITORY "https://github.com/gabime/spdlog" CACHE STRING "Repository of spdlog")
 set(SPDLOG_TAG "fa659bf7ad327a13e2e7134d1004d0cda0c3ed96" CACHE STRING "Git tag/branch of spdlog")
