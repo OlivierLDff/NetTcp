721cbf1334e5e7a7c8d15b14cfaeb6bb0c7b7965
William Hamon <william.hamon35@gmail.com>
2022-02-25 11:53:35 +0100

    🔨 correct CPM cache

--- cmake/FetchGTest.cmake
+++ cmake/FetchGTest.cmake
@@ -1,15 +1,33 @@
-include(FetchContent)
+include(cmake/CPM.cmake)
 
-set(GTEST_REPOSITORY "https://github.com/google/googletest" CACHE STRING "googletest repository url")
-set(GTEST_TAG "master" CACHE STRING "googletest git tag")
-
-FetchContent_Declare(
-  googletest
-  GIT_REPOSITORY ${GTEST_REPOSITORY}
-  GIT_TAG        ${GTEST_TAG}
+set(GTEST_REPOSITORY
+    "https://github.com/google/googletest"
+    CACHE STRING "googletest repository url"
+)
+set(GTEST_TAG
+    "release-1.11.0"
+    CACHE STRING "googletest git tag"
 )
 
-set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
-FetchContent_MakeAvailable(googletest)
+CPMAddPackage(
+  NAME googletest
+  GIT_REPOSITORY ${GTEST_REPOSITORY}
+  GIT_TAG ${GTEST_TAG}
+  OPTIONS "gtest_force_shared_crt ON"
+)
 
-set_target_properties(gtest gtest_main gmock gmock_main PROPERTIES FOLDER "Dependencies/gtest")
+if(TARGET gtest)
+  set_target_properties(gtest PROPERTIES FOLDER "Dependencies/gtest")
+endif()
+
+if(TARGET gtest_main)
+  set_target_properties(gtest_main PROPERTIES FOLDER "Dependencies/gtest")
+endif()
+
+if(TARGET gmock)
+  set_target_properties(gmock PROPERTIES FOLDER "Dependencies/gtest")
+endif()
+
+if(TARGET gmock_main)
+  set_target_properties(gmock_main PROPERTIES FOLDER "Dependencies/gtest")
+endif()
