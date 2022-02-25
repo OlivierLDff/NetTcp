c726d46d29b4a7db6a8bffb4781f0b0d7442c12f
William Hamon <william.hamon35@gmail.com>
2022-02-25 14:06:58 +0100

    🔨 change from FetchContent to CPMAddPackage in order to not being in conflict with CPM Cache

--- cmake/FetchQOlm.cmake
+++ cmake/FetchQOlm.cmake
@@ -1,12 +1,11 @@
-include(FetchContent)
+include(cmake/CPM.cmake)
 
 set(QOLM_REPOSITORY "https://github.com/OlivierLDff/QOlm.git" CACHE STRING "QOlm repository url")
 set(QOLM_TAG master CACHE STRING "QOlm git tag")
 
-FetchContent_Declare(
-  QOlm
+CPMAddPackage(
+  NAME qolm
   GIT_REPOSITORY ${QOLM_REPOSITORY}
-  GIT_TAG        ${QOLM_TAG}
+  GIT_TAG ${QOLM_TAG}
 )
 
-FetchContent_MakeAvailable(QOlm)
