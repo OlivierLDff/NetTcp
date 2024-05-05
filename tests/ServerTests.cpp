c989949d128e0d76404c2dd3def6c72f84be3535
Olivier LDff <olivier.ldff@gmail.com>
2024-05-05 20:37:25 +0200

    better logs

--- tests/ServerTests.cpp
+++ tests/ServerTests.cpp
@@ -114,7 +114,7 @@ public:
         // Give time to finish last connection
         QTest::qWait(1000);
         qInfo() << "Connected Count : " << connectedCount;
-        ASSERT_TRUE(connectedCount > 40);
+        ASSERT_GT(connectedCount, 40);
         ASSERT_EQ(newClientSpy.count(), connectedCount);
     }
 };
